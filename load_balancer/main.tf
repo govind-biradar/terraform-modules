d897a3ca3c9a18d3d6819c24deb9e03214db873e
govind-biradar <149034916+govind-biradar@users.noreply.github.com>
2024-06-07 11:44:05 +0530

    push

--- load_balancer/main.tf
+++ load_balancer/main.tf
@@ -1,7 +1,6 @@
 resource "aws_lb" "this" {
   name               = var.lb_name
   internal           = var.lb_internal
-  load_balancer_type = var.lb_type
   subnets            = var.lb_subnets
   security_groups    = [var.lb_security_group]
 
