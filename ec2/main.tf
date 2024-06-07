d58365576e18c8a7dbe2f32163c854ec0ae1e570
govind-biradar <149034916+govind-biradar@users.noreply.github.com>
2024-06-07 10:38:13 +0530

    push

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,9 +2,10 @@ resource "aws_instance" "this" {
   for_each      = { for idx, cfg in var.instance_configs : idx => cfg }
   ami           = each.value.ami_id
   instance_type = each.value.instance_type
-  subnet_id     = var.subnet_ids[each.key % length(var.subnet_ids)]  # Distribute instances across subnets
+  subnet_id     = var.subnet_ids[each.key % length(var.subnet_ids)] 
   key_name      = each.value.key_name
   vpc_security_group_ids = [var.security_group]
+  associate_public_ip_address = true
 
   tags = {
     Name = each.value.instance_name
